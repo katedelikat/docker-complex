038e461881c66ed2f128032a397eec4f52a897d8
katedelikat <kate.delikat@gmail.com>
2021-11-29 11:54:08 +0200

    nginx dockerfile

--- server/keys.js
+++ server/keys.js
@@ -1,9 +1,13 @@
 module.exports = {
     redisHost: process.env.REDIS_HOST,
     redisPort: process.env.REDIS_PORT,
-    pgUser: process.env.PGUSER,
+    
     pgHost: process.env.PGHOST,
-    pgDatabase: process.env.PGDATABASE,
+    pgPort: process.env.PGPORT,
+
+    pgUser: process.env.PGUSER,
     pgPassword: process.env.PGPASSWORD,
-    pgPort: process.env.PGPORT
+    
+    pgDatabase: process.env.PGDATABASE    
+    
 };
